9432a376bc0439bacd809f8b9e8c62de6e2e245c
Prinsa Gandhi <prinsahg@gmail.com>
2020-12-19 19:09:04 -0500

    3

---
+++ DataAnalysis/analysis.R
@@ -0,0 +1,49 @@
+#data analysis work
+library(kableExtra)
+library(imputeMissings)
+
+
+
+survey_data <- read.csv("F:/sta304/final/survey1.csv")
+
+survey_data <- as.data.frame(survey_data)
+#checking missing values
+
+is.na(survey_data)
+(colSums(is.na(survey_data)))
+
+miss<- (colSums(is.na(survey_data)))/491775*100
+miss<- as.data.frame(miss)
+miss
+
+survey_data2<- survey_data[!is.na(survey_data$obese), ]
+
+miss<- (colSums(is.na(survey_data2)))/465046*100
+miss<- as.data.frame(miss)
+miss
+
+survey_data3<- survey_data2[-c(3,16,24,44,45,46,63,64,66,68,69,70,71,72,73,74,
+                               79,80,81,82,83,84,85,86,87,88,89,90,91,
+                               92,93,94,95,96,97,98,99,100,101,102,103,104,
+                               105)]
+
+miss<- (colSums(is.na(survey_data3)))/465046*100
+miss<- as.data.frame(miss)
+miss
+
+#if omit rows with NA values
+survey_data4 <- na.omit(survey_data3)
+
+#impute missing variables
+data<- impute(survey_data3,object=NULL, method="median/mode") #median mode imputation
+
+
+
+
+
+
+
+
+
+
+
