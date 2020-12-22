d7971c80268c51ed5ad4e3b8f6c746995598e98a
Prinsa Gandhi <prinsahg@gmail.com>
2020-12-22 17:40:02 -0500

    1

--- datacleaning.R
+++ datacleaning.R
@@ -1,13 +1,10 @@
 #### Preamble ####
-# Purpose: Prepare and clean the survey data downloaded from [...UPDATE ME!!!!!]
-# Author: Rohan Alexander and Sam Caetano [CHANGE THIS TO YOUR NAME!!!!]
-# Data: 22 October 2020
-# Contact: rohan.alexander@utoronto.ca [PROBABLY CHANGE THIS ALSO!!!!]
+# Purpose: Prepare and clean the survey data downloaded from kaggle(originally BRFSS website )
+# Author: Prinsa Gandhi
+# Data: 22 December 2020
+# Contact: prinsa.gandhi@mail.utoronto.ca
 # License: MIT
-# Pre-requisites: 
-# - Need to have downloaded the data from X and save the folder that you're 
-# interested in to inputs/data 
-# - Don't forget to gitignore it!
+
 
 
 #### Workspace setup ####
